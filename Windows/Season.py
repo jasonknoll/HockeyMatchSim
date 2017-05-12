b4b0fad1a127d2c1d642934e6465d817d63e5594
jasonknoll <jasonknoll98@aol.com>
2017-05-12 10:21:25 -0600

    Adding More to the Basics of a Season

--- Windows/Season.py
+++ Windows/Season.py
@@ -1,13 +1,21 @@
 #Possibly create an array of every match
 #Declare the winner without playoffs based off of points
-#May not need this class
+
+from Match import *
 
 class Season: 
 	def __init__(self):
 		self.matches = [] #array of every match (2,460 matches)
 
 	def createMatch(self, t1, t2):
-		pass
+		i = 0
+		while (i < 82):
+			self.matches.append(Match(t1, t2)) #create the match (make a loop to make 82 of them) 0-81
+		"""
+		i = 0
+		while (i < len(matches)):
+			matches[i].run() #make a run function without printing the scores
+		"""
 		#add the game to the 2 team's schedule.
 
 	def runSeason(self):
