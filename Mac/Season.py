1876e944cf2d75821c017082746574e0aa62e59a
Jason Knoll <jasonknoll98@aol.com>
2017-08-09 12:31:16 -0600

    Removed Mac Folder

--- Mac/Season.py
+++
@@ -1,7 +0,0 @@
-#Possibly create an array of every match
-#Declare the winner without playoffs based off of points
-#May not need this class
-
-class Season: 
-	def __init__(self):
-		self.matches = [] #array of every match (2,460 matches)
